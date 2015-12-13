4740cf3d9e3e5de201db3be8987d0f86978aaf25
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2015-12-13 15:27:58 +0100

    Convert README to rst format

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-readme = open('README.md').read()
+readme = open('README.rst').read()
 import blinker
 version = blinker.__version__
 
