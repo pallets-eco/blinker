3274fb3ed5a032cd5bc1e5d224de14d1775ba36f
pgjones <philip.graham.jones@googlemail.com>
2022-07-23 11:49:17 +0100

    Remove unicode compat
    Only Python3 is targetted therefore this isn't required.

--- blinker/_utilities.py
+++ blinker/_utilities.py
@@ -40,18 +40,12 @@ class symbol(object):
             return cls.symbols.setdefault(name, _symbol(name))
 
 
-try:
-    text = (str, unicode)
-except NameError:
-    text = str
-
-
 def hashable_identity(obj):
     if hasattr(obj, '__func__'):
         return (id(obj.__func__), id(obj.__self__))
     elif hasattr(obj, 'im_func'):
         return (id(obj.im_func), id(obj.im_self))
-    elif isinstance(obj, text):
+    elif isinstance(obj, str):
         return obj
     else:
         return id(obj)
