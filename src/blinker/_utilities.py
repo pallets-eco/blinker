0d4ca6e72c155e30aedd4315e8678ee9cada32b4
pgjones <philip.graham.jones@googlemail.com>
2023-01-22 14:09:48 +0000

    Allow int senders
    This allows int values as senders much in the same way string values
    are allowed.

--- src/blinker/_utilities.py
+++ src/blinker/_utilities.py
@@ -47,7 +47,7 @@ def hashable_identity(obj):
         return (id(obj.__func__), id(obj.__self__))
     elif hasattr(obj, "im_func"):
         return (id(obj.im_func), id(obj.im_self))
-    elif isinstance(obj, str):
+    elif isinstance(obj, (int, str)):
         return obj
     else:
         return id(obj)
