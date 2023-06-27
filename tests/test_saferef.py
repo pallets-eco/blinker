5eb00b67608d16ace34445b35353d1cafd0f4bb5
David Lord <davidism@gmail.com>
2023-06-27 08:58:21 -0700

    update dependencies

--- tests/test_saferef.py
+++ tests/test_saferef.py
@@ -52,7 +52,6 @@ class _Sample3:
 
 
 class TestSaferef(unittest.TestCase):
-
     # XXX: The original tests had a test for closure, and it had an
     # off-by-one problem, perhaps due to scope issues.  It has been
     # removed from this test suite.
